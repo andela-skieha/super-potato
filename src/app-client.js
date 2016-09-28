640e928027a482e73dd5f2062fddd23bfbf10687
Njeri Kieha <sylvia.kieha@andela.com>
2016-09-28 11:23:10 +0300

    Application entry point

---
+++ src/app-client.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRoutes from './components/AppRoutes';
+
+window.onload = () => {
+  ReactDOM.render((
+    <AppRoutes />
+  ),
+  document.getElementById('main')
+);
+};
